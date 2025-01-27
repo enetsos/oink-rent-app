f4948ee7c20688c2912d12e7f8daa0001cc7ca68
Walter Losa <jetw.ncc@gmail.com>
2025-01-27 17:36:15 +0100

    connection to db

--- backend/Entities/User.cs
+++ backend/Entities/User.cs
@@ -7,6 +7,7 @@ namespace backend.Entities
 {
     public class User
     {
+        public Guid Id { get; set; }
         public string Username { get; set; } = string.Empty;
         public string PasswordHash { get; set; } = string.Empty;
     }
